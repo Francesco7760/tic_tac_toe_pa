1a416532fe510ee9c319e60052977524c1e132aa
francesco <dibernardofrancesco93@virgilio.it>
2023-06-10 19:50:51 +0200

    index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 const express = require('express');
-import * as middleware from './src/middleware/middleware_jwt';
-import * as controller_game from './src/controller/controller_game';
-import * as controller_user from './src/controller/controller_user';
-import * as controller_move from './src/controller/controller_move';
+import * as middleware from './components/middleware/middleware_jwt';
+import * as Controller_game from './components/controller/controller_game';
+import * as Controller_user from './components/controller/controller_user';
+import * as Controller_move from './components/controller/controller_move';
 
 // Network constants
 const PORT = 8080;
@@ -12,45 +12,45 @@ const app = express();
 app.use(express.json());
 
 // validazione e check token
-app.use([
-    middleware.checkHeader,
-    middleware.checkToken,
-    middleware.verifyAndAuthenticate,
-    middleware.checkJwtPayload
-]);
+//app.use([
+//    middleware.checkHeader,
+//    middleware.checkToken,
+//    middleware.verifyAndAuthenticate,
+//    middleware.checkJwtPayload
+//]);
 
 // rotta per creare una nuova partita
 app.post('/creapartita',(req:any, res:any) => {
-    controller_game.CreateNewGame(req,res);
+    Controller_game.CreateNewGame(req,res);
 });
 // rotta per creare una uova mossa
 app.post('/creanuovamossa', (req:any, res:any) => {
-    controller_move.CreateMove(req,res);
+    Controller_move.CreateMove(req,res);
 });
 
 // rotta per abbandonare partita
 app.post('/abbandonapartita', (req:any, res:any) => {
-    controller_game.AbbandonedGame(req,res);
+    Controller_game.AbbandonedGame(req,res);
 });
 
 // rotta per valutare stati di una data partita
 app.get('/statopartita', (req:any, res:any) => {
-    controller_game.ShowInfoGame(req,res);
+    Controller_game.ShowInfoGame(req,res);
 });
 
 // rotta per per ritornare storico mosse parita
 app.get('/storicomosse', (req:any, res:any) => {
-    controller_move.ShowMoves(req,res);
+    Controller_move.ShowMoves(req,res);
 });
 
 // rotta per per restituire classifica giocatori
 app.get('/classifica', (req:any, res:any) => {
-    controller_user.ShowUserRanking(req,res);
+    Controller_user.ShowUserRanking(req,res);
 });
 
 // rotta per aggiungere token a giocatore
 app.post('/aggiungitoken', (req:any, res:any) => {
-    controller_user.AddToken(req,res);
+    Controller_user.AddToken(req.body.player,req.body.token,res);
 });
 
 app.listen(PORT, HOST);
